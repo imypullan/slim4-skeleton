6a4d5af17066db3237c1c2aca57a287b57d54a50
Imy <imypullan@gmail.com>
2021-05-21 15:26:07 +0100

    adding return bools to PDO queries

--- src/Models/TasksModel.php
+++ src/Models/TasksModel.php
@@ -13,16 +13,16 @@ class TasksModel {
     /**
      * @return DB array
      */
-    public function getUncompletedTasks()
+    public function getUncompletedTasks(): array
     {
         $query = $this->db->prepare('SELECT * FROM `todo-list` WHERE `completed` = 0 AND `deleted` = 0;');
         $query->execute();
         return $query->fetchAll();
     }
     /**
-     * @return array
+     * @return DB array
      */
-    public function getCompletedTasks()
+    public function getCompletedTasks(): array
     {
         $query = $this->db->prepare('SELECT * FROM `todo-list` WHERE `completed` = 1 AND `deleted` = 0;');
         $query->execute();
@@ -31,36 +31,48 @@ class TasksModel {
 
     /**
      * @param $newTask
+     * @return bool
      */
-    public function addTask($newTask)
+    public function addTask($newTask): bool
     {
         $query = $this->db->prepare('INSERT INTO `todo-list` (`task_name`) VALUES (:task_name);');
         $query->bindParam(':task_name', $newTask);
-        $query->execute();
+        if($query->execute()) {
+            return true;
+        }
+        return false;
     }
     /**
      * @param $completedTask
+     * @return bool
      */
-    public function markTaskAsCompleted($completedTask)
+    public function markTaskAsCompleted($completedTask): bool
     {
         $query = $this->db->prepare('UPDATE `todo-list` SET `completed` = 1 WHERE `id` = :id;');
         $query->bindParam(':id', $completedTask);
-        $query->execute();
+        if($query->execute()) {
+            return true;
+        }
+        return false;
     }
     /**
      * @param $deletedTask
+     * @return bool
      */
-    public function deleteTask($deletedTask)
+    public function deleteTask($deletedTask): bool
     {
         $query = $this->db->prepare('UPDATE `todo-list` SET `deleted` = 1 WHERE `id` = :id;');
         $query->bindParam(':id', $deletedTask);
-        $query->execute();
+        if($query->execute()) {
+            return true;
+        }
+        return false;
     }
     /**
      * @param $taskForEdit
      * @return array
      */
-    public function getTaskForEdit($taskForEdit)
+    public function getTaskForEdit($taskForEdit): array
     {
         $query = $this->db->prepare('SELECT `id`, `task_name`, `completed` FROM `todo-list` WHERE `id` = :id;');
         $query->bindParam(':id', $taskForEdit);
@@ -69,12 +81,16 @@ class TasksModel {
     }
     /**
      * @param $taskForEdit
+     * @return bool
      */
-    public function updateDB($taskForEdit)
+    public function updateDB($taskForEdit): bool
     {
         $query = $this->db->prepare('UPDATE `todo-list` SET `task_name` = :task_name WHERE `id` = :id;');
         $query->bindParam(':id', $taskForEdit['id']);
         $query->bindParam(':task_name', $taskForEdit['task_name']);
-        $query->execute();
+        if($query->execute()) {
+            return true;
+        }
+        return false;
     }
 }
